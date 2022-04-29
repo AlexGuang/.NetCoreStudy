0987289d6165eca67507d474a30fcc4c65890f0a
xiaoguangli <guangmvp@gmail.com>
2022-04-29 21:29:22 +1000

    ASP.NET CORE static file

--- StudentManagement/StudentManagement/Startup.cs
+++ StudentManagement/StudentManagement/Startup.cs
@@ -30,12 +30,30 @@ namespace StudentManagement
             {
                 app.UseDeveloperExceptionPage();
             }
+            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();//注册默认网页
+
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("52ab.html");
+
+
+            //添加默认文件中间件
+            //app.UseDefaultFiles();
+            //添加静态文件中间件
+            //app.UseStaticFiles();
+           
+            //index.html index.htm 默认 default.html default.htm
+
+            FileServerOptions fileServerOptions = new FileServerOptions();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("52ab.html");
+
+            app.UseFileServer(fileServerOptions);
+
+
 
             app.Run(async (context) =>
             {
-                var configVal = _config["MyKey"];
-
-                await context.Response.WriteAsync(configVal);
+               await context.Response.WriteAsync("Hello world");
             });
         }
     }
