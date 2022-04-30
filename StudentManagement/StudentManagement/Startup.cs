640206985f25aa7688c2b20c026c1903d2f89eb9
xiaoguangli <guangmvp@gmail.com>
2022-04-30 12:55:46 +1000

    Depedence Injection through the Configure serviceDI container

--- StudentManagement/StudentManagement/Startup.cs
+++ StudentManagement/StudentManagement/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StudentManagement.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,7 @@ namespace StudentManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IStudentRepository,MockStudentRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
