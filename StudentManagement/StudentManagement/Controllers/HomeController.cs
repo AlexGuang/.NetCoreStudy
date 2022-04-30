31693f3cd2d1371609b08a88744fdc21d73002d1
xiaoguangli <guangmvp@gmail.com>
2022-04-30 11:38:34 +1000

    Add a Home controller

---
+++ StudentManagement/StudentManagement/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello world from MVC";
+
+        }
+    }
+}
--- StudentManagement/StudentManagement/Startup.cs
+++ StudentManagement/StudentManagement/Startup.cs
@@ -21,29 +21,32 @@ namespace StudentManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsEnvironment("UAT"))//如果开发环境是用户验收环境
-            {
-                app.UseDeveloperExceptionPage();//可以显示开发者异常页面
-            }
-            else if (env.IsDevelopment())
-            {
-                #region 开发者异常界面
-                //  DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions();
-                // developerExceptionPageOptions.SourceCodeLineCount = 10;//对报错附近的代码显示，能够快速定位
-                //  app.UseDeveloperExceptionPage(developerExceptionPageOptions);
-                #endregion
-                app.UseDeveloperExceptionPage();
-            }
-            else if (env.IsStaging()||env.IsProduction()||env.IsEnvironment("UAT"))
-            {
-                app.UseExceptionHandler("/Error");
+            #region 判断开发者环境和处理开发者异常界面
+            //if (env.IsEnvironment("UAT"))//如果开发环境是用户验收环境
+            //{
+            //    app.UseDeveloperExceptionPage();//可以显示开发者异常页面
+            //}
+            //else if (env.IsDevelopment())
+            //{
+            //    #region 开发者异常界面
+            //    //  DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions();
+            //    // developerExceptionPageOptions.SourceCodeLineCount = 10;//对报错附近的代码显示，能够快速定位
+            //    //  app.UseDeveloperExceptionPage(developerExceptionPageOptions);
+            //    #endregion
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else if (env.IsStaging()||env.IsProduction()||env.IsEnvironment("UAT"))
+            //{
+            //    app.UseExceptionHandler("/Error");
 
-            }
+            //}
+            #endregion
             #region 中间件和静态文件学习
             //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();//注册默认网页
 
@@ -64,14 +67,18 @@ namespace StudentManagement
 
             //app.UseFileServer(fileServerOptions); 终端中间件
             #endregion
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseStaticFiles();
 
-
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
                // throw new Exception("您的轻轻在管道中发生了一些错误，请检查");                
-                await context.Response.WriteAsync("Hosting Environment:"+env.EnvironmentName);
+                await context.Response.WriteAsync("Hello world!");
             });
         }
     }
