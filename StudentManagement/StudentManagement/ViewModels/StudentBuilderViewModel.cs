8a2d1b601948ce4266eeb3a710725d81976d405c
xiaoguangli <guangmvp@gmail.com>
2022-05-04 23:47:25 +1000

    Add uploading input form

---
+++ StudentManagement/StudentManagement/ViewModels/StudentBuilderViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using StudentManagement.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.ViewModels
+{
+    public class StudentBuilderViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "请输入您的名字"), MaxLength(50, ErrorMessage = "名字的长度不能超过50个字符")]
+        [Display(Name = "姓名")]
+        public string Name { get; set; }
+        [Display(Name = "班级信息")]
+        [Required(ErrorMessage = "请选择班级信息")]
+        public ClassNameEnum? ClassName { get; set; }
+        [Display(Name = "电子邮件")]
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$",
+            ErrorMessage = "邮箱的格式不正确")]
+        [Required(ErrorMessage = "请输入邮箱地址")]
+        public string Email { get; set; }
+        [Display(Name="图片")]
+        public IFormFile Photo { get; set; }
+    }
+}
